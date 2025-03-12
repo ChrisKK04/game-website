222fae6f2fe70f097e7ec98b7fea29f05db035d5
Kristjan Kaasik <155821871+ChrisKK04@users.noreply.github.com>
2025-03-12 19:09:09 +0200

    Add basic homepage

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # This file handles routing
+
+from flask import Flask
+from flask import redirect, render_template, request, session
+import config, db
+
+app = Flask(__name__)
+app.secret_key = config.secret_key # setting session key
+
+@app.route("/") # homepage
+def index():
+    return render_template("index.html")
