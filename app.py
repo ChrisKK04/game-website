7867c7e729e1b39993bc412161f3decd698780c9
Kristjan Kaasik <155821871+ChrisKK04@users.noreply.github.com>
2025-05-17 15:35:59 +0300

    Add session functionality

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, session
+import config
+import db
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
---
+++ config.py
@@ -0,0 +1,2 @@
+# This is the session key
+secret_key = "d751a7b727f62a1c45aa32e611eddbb2"
