9cc286bbd271482d051e3faab093e3c647acad86
Kristjan Kaasik <155821871+ChrisKK04@users.noreply.github.com>
2025-05-18 12:42:27 +0300

    Update schema.sql with tables for users, games and reviews

--- schema.sql
+++ schema.sql
@@ -1,1 +1,27 @@
 -- Includes the database layout for the website
+
+-- Table for the users
+CREATE TABLE Users (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
+
+-- Table for the games
+CREATE TABLE Games (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES Users(id),
+    description TEXT,
+    uploaded_at TEXT
+);
+
+-- Table for the reviews
+CREATE TABLE Reviews (
+    id INTEGER PRIMARY KEY,
+    content TEXT,
+    score INTEGER,
+    sent_at TEXT,
+    user_id INTEGER REFERENCES Users(id),
+    game_id INTEGER REFERENCES Games(id)
+);
