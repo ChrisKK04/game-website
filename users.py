b51a35e951e6cfba135abc959838225e76f3d7e2
Kristjan Kaasik <155821871+ChrisKK04@users.noreply.github.com>
2025-05-28 16:32:31 +0300

    Show scores, amount of reviews and average scores in user pages

--- users.py
+++ users.py
@@ -9,17 +9,19 @@ def get_user(user_id): # users username and developer status
     return result[0] if result else None
 
 def get_reviews(user_id): # users given reviews
-    sql = """SELECT R.id, R.game_id, G.title AS game_title, R.sent_at, R.content
+    sql = """SELECT R.id, R.game_id, G.title AS game_title, R.sent_at, R.content, R.score
             FROM Reviews R, Games G
             WHERE G.id = R.game_id AND R.user_id = ?
             ORDER BY R.sent_at DESC"""
     return db.query(sql, [user_id])
 
 def get_games(user_id): # developers published games
-    sql = """SELECT id, title, uploaded_at, description
-            FROM Games
-            WHERE user_id = ?
-            ORDER BY uploaded_at DESC"""
+    sql = """SELECT G.id, G.title, G.description, COUNT(R.id) total, G.uploaded_at, G.user_id, U.username,
+            ROUND(1.0*SUM(R.score) / COUNT(R.id), 1) AS average
+            FROM Games G LEFT JOIN Reviews R ON G.id = R.game_id, Users U
+            WHERE G.user_id = U.id AND G.user_id = ?
+            GROUP BY G.id
+            ORDER BY G.id DESC"""
     return db.query(sql, [user_id])
 
 def update_profile_picture(user_id, image): # updates the users profile picture
