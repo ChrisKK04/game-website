d3f2a56aa6cc780b33e62035119c73162b5414dc
Kristjan Kaasik <155821871+ChrisKK04@users.noreply.github.com>
2025-06-06 16:59:19 +0300

    Add python script pre_data.py for testing the website
    pre_data.py is a script that will empty the database and populate
    it with some users, games and reviews. It tries to mimic how users
    would use the website.

---
+++ pre_data.py
@@ -0,0 +1,62 @@
+# This is a python script that inserts custom data into database.db for testing.
+# the script doesn't include images for users or games
+# database.db and the tables in it have to exist before running the script
+    # if database.db doesn't exist:
+        # $ sqlite3 database.db < schema.sql
+        # $ sqlite3 database.db < init.sql
+
+import sqlite3
+from werkzeug.security import generate_password_hash
+
+db = sqlite3.connect("database.db")
+
+db.execute("DELETE FROM Users")
+db.execute("DELETE FROM Games")
+db.execute("DELETE FROM Reviews")
+
+users = [["RampageGames", "rampagegames", 1],
+         ["Pipe", "pipe", 1],
+         ["HeroicGames", "heroicgames", 1],
+         ["Jason", "jason", 0],
+         ["Emily", "emily", 0]]
+
+games = [["Falorant", """Falorant is a team-based first-person tactical hero shooter set in the near future.
+          Players play as one of a set of Agents, characters based on several countries and cultures
+          around the world. In the main game mode, players are assigned to either the attacking or
+          defending team with each team having five players on it.""", 1],
+          ["League of Ripoffs", """League of Ripoffs is a team-based strategy game where two teams of
+           five powerful champions face off to destroy the other's base. Choose from over 140
+           champions to make epic plays, secure kills, and take down towers as you battle
+           your way to victory.""", 1],
+          ["Counter the DMCA 2", """Welcome to Counter the DMCA 2, the next evolution of the world's
+           most legendary competitive shooter. Built on the powerful Closing 2 engine, CDMCA2 brings
+           breathtaking visuals, hyper-responsive gameplay, and reimagined classics to life — all
+           for free.""", 2],
+           ["Fortress Fight", """Join the worldwide sensation that blends high-speed combat with
+            creative construction. In FortressFight Battle Royale, 100 players fight to be the last
+            one standing — scavenging weapons, building defenses, and outlasting the competition
+            in a constantly evolving world.""", 3]]
+
+reviews = [["I love the new lighting and gun feel!", 4, 3, 5],
+           ["Similar to CDMCA 2 but with abilities.", 4, 1, 3],
+           ["wayyyyyyy better than CDMCA 2", 5, 1, 5],
+           ["The game has way too many characters.", 5, 2, 2]]
+
+for username, password, developer in users: # inserts the users
+    password_hash = generate_password_hash(password)
+    db.execute("""INSERT INTO Users (username, password_hash, developer)
+                  VALUES (?, ?, ?)""",
+                  [username, password_hash, developer])
+
+for title, description, user_id in games: # inserts the games
+    db.execute("""INSERT INTO Games (title, description, uploaded_at, user_id)
+                  VALUES (?, ?, datetime('now'), ?)""",
+                  [title, description, user_id])
+
+for content, user_id, game_id, score in reviews: # inserts the reviews
+    db.execute("""INSERT INTO Reviews (content, sent_at, user_id, game_id, score)
+                  VALUES (?, datetime('now'), ?, ?, ?)""",
+                  [content, user_id, game_id, score])
+    
+db.commit()
+db.close()
